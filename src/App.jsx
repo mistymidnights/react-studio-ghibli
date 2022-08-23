6a61e30e9bc7791a031d18f125a6f10f9796098a
mistymidnights <mistydeveloping@gmail.com>
2022-08-23 20:16:01 +0200

    added lightmode

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import "./App.css";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
 import Footer from "./components/Footer";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div>
+    <div className="entireApp" data-theme={theme}>
+      <button onClick={switchTheme}>Color mode</button>
       <Header />
       <Movies />
       <Footer />
